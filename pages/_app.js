cff0efbad30f8be7b7af4dc87929ba8b541df8a4
Eric Lin <eric.lin@consensys.net>
2022-03-13 16:45:49 +1000

    add Dockerfile and fix theme

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "@fullcalendar/common/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
