82c7d13463c1651cbfc4433e705f54ce7614f099
Eric Lin <eric.lin@consensys.net>
2022-04-13 14:17:13 +1000

    change colors

--- styles/theme.js
+++ styles/theme.js
@@ -22,83 +22,84 @@ export const StyleWrapper = styled.div`
   .fc-toolbar-title {
     font-size: 1.5em;
   }
-  .fc-button {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-300)"
-        : "var(--chakra-colors-red-300)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-300)"
-        : "var(--chakra-colors-red-300)"};
-  }
-  .fc-next-button:hover {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-  }
-  .fc-button-primary:not(:disabled):active {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-400)"
-        : "var(--chakra-colors-red-400)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-400)"
-        : "var(--chakra-colors-red-400)"};
-  }
-  .fc-icon-chevron-right:active {
-    color: #ffffff;
-  }
-  .fc-icon-chevron-left:active {
-    color: #ffffff;
-  }
-  .fc-prev-button:hover {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-  }
-  .fc-button-primary:hover {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-  }
-  .fc-button-primary:disabled {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-200)"
-        : "var(--chakra-colors-red-200)"};
-  }
-  .fc-button-primary:not(:disabled).fc-button-active {
-    background-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-900)"
-        : "var(--chakra-colors-red-900)"};
-    border-color: ${(props) =>
-      props.colorMode === "dark"
-        ? "var(--chakra-colors-red-900)"
-        : "var(--chakra-colors-red-900)"};
-  }
 `;
+
+// .fc-button {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-300)"
+//         : "var(--chakra-colors-red-300)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-300)"
+//         : "var(--chakra-colors-red-300)"};
+//   }
+//   .fc-next-button:hover {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//   }
+//   .fc-button-primary:not(:disabled):active {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-400)"
+//         : "var(--chakra-colors-red-400)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-400)"
+//         : "var(--chakra-colors-red-400)"};
+//   }
+//   .fc-icon-chevron-right:active {
+//     color: #ffffff;
+//   }
+//   .fc-icon-chevron-left:active {
+//     color: #ffffff;
+//   }
+//   .fc-prev-button:hover {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//   }
+//   .fc-button-primary:hover {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//   }
+//   .fc-button-primary:disabled {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-200)"
+//         : "var(--chakra-colors-red-200)"};
+//   }
+//   .fc-button-primary:not(:disabled).fc-button-active {
+//     background-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-900)"
+//         : "var(--chakra-colors-red-900)"};
+//     border-color: ${(props) =>
+//       props.colorMode === "dark"
+//         ? "var(--chakra-colors-red-900)"
+//         : "var(--chakra-colors-red-900)"};
+//   }
 
 // 3. extend the theme
 const theme = extendTheme({ config });
